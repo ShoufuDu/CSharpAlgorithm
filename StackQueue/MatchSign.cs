31af25323cd3be6153d9f2d76f184bf1dcaf7fce
ShoufuDu <dushoufu@gmail.com>
2019-07-17 22:34:57 +1000

    format

--- StackQueue/MatchSign.cs
+++ StackQueue/MatchSign.cs
@@ -1,66 +1,58 @@
-
 using System.Collections.Generic;
 using CSharpAlgorithm.Util;
 
-namespace CSharpAlgorithm.StackQueue
-{
-    public class StackQueueSolution
-    {
-        public static Dictionary<char,char> signs = new Dictionary<char,char>{
-                        {'(',')'},
-                        {'[',']'},
-                        {'{','}'}
-                        };
+namespace CSharpAlgorithm.StackQueue {
+
+    // Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+    // The brackets must close in the correct order, "()" and "()[]" are all valid but "(]" and "([)]" are not.
+    public class StackQueueSolution {
+        public static Dictionary<char, char> signs = new Dictionary<char, char> { { '(', ')' },
+            { '[', ']' },
+            { '{', '}' }
+        };
         public static bool MatchSignIsValid (string s) {
 
-                Stack<char> stack = new Stack<char>();
+            Stack<char> stack = new Stack<char> ();
 
-                stack.Push(s[0]);
+            stack.Push (s[0]);
 
-                for(int i=1;i<s.Length;i++)
-                {
-                    if (stack.Count == 0)
-                        {
-                            stack.Push(s[i]);
-                            continue;
-                        }
-
-                    if(IsMatch(stack.Peek(),s[i]))
-                    {
-                        stack.Pop();
-                    }
-                    else
-                    {
-                        stack.Push(s[i]);
-                    }
+            for (int i = 1; i < s.Length; i++) {
+                if (stack.Count == 0) {
+                    stack.Push (s[i]);
+                    continue;
                 }
 
-                if (stack.Count == 0)
-                    return true;
-                else
-                    return false;
+                if (IsMatch (stack.Peek (), s[i])) {
+                    stack.Pop ();
+                } else {
+                    stack.Push (s[i]);
+                }
+            }
+
+            if (stack.Count == 0)
+                return true;
+            else
+                return false;
 
         }
 
-        public static bool IsMatch(char a,char b)
-        {
+        public static bool IsMatch (char a, char b) {
             char c;
-            if (signs.TryGetValue(a,out c)&&c==b)
+            if (signs.TryGetValue (a, out c) && c == b)
                 return true;
             else
                 return false;
         }
 
-        public static void TestMatchSign(){
+        public static void TestMatchSign () {
             const string testString = "()()[{()}]";
-            Expect.Expect1("TestMatchSign",MatchSignIsValid, testString, true);
+            Expect.Expect1 ("TestMatchSign", MatchSignIsValid, testString, true);
 
             const string testStr1 = "[(]}";
-            Expect.Expect1("TestMatchSign",MatchSignIsValid, testStr1, false);
+            Expect.Expect1 ("TestMatchSign", MatchSignIsValid, testStr1, false);
 
             const string testStr3 = "[{[()]}]";
-            Expect.Expect1("TestMatchSign",MatchSignIsValid, testStr3, true);
-
+            Expect.Expect1 ("TestMatchSign", MatchSignIsValid, testStr3, true);
         }
 
     }
