336d25815f5c07aafab129a150804da4819d34ff
ShoufuDu <dushoufu@gmail.com>
2019-07-20 22:23:36 +1000

    longmatch_2

--- StackQueue/LongMatchSign.cs
+++ StackQueue/LongMatchSign.cs
@@ -1,4 +1,6 @@
-using System.Dynamic;
+using System.Linq;
+using System;
+using System.Collections.Generic;
 using CSharpAlgorithm.Util;
 
 namespace CSharpAlgorithm.StackQueue {
@@ -8,6 +10,7 @@ namespace CSharpAlgorithm.StackQueue {
             // For "(()", the longest valid parentheses substring is "()", which has length = 2.
             // Another example is ")()())", where the longest valid parentheses substring is "()()", which has length = 4.
 
+            // Method 1: The brute-force
              static public int LongMatchSign_1 (string s) {
                 int maxLen = 0;
                 for(int i=0;i<s.Length;i++)
@@ -20,11 +23,48 @@ namespace CSharpAlgorithm.StackQueue {
                 return maxLen;
             }
 
+            // Method 2: Stack
+            static public int LongMatchSign_2(String s){
+                int maxLen = 0;
+                int last = -1; // used to track the last unmatched ')'
+                Stack<int> signStack = new Stack<int>();
+
+                for(int i=0;i<s.Length;i++)
+                {
+                    if(s[i]=='('){
+                        signStack.Push(i);
+                    }
+                    else
+                    {
+                        if(signStack.Count == 0)
+                        {
+                            last = i; //record the last ')'
+                        }
+                        else // find a match
+                        {
+                            signStack.Pop();
+                            if(signStack.Count==0)
+                            {
+                                maxLen = Math.Max(maxLen,i-last);
+                            }
+                            else
+                            {
+                                maxLen = Math.Max(maxLen,i-signStack.Peek());
+                            }
+                        }
+                    }
+                }
+
+                return maxLen;
+            }
+
             static public void Test_LongMatchSign_1()
             {
                 string s1 = "()()()))(((()))))";
 
-                Expect.Expect1("Test_LongMatchSign_1",LongMatchSign_1,s1,6);
+                Expect.Expect1("Test LongMatchSign_1",LongMatchSign_1,s1,8);
+
+                Expect.Expect1("Test LongMatchSign_2",LongMatchSign_2,s1,8);
             }
         }
 }
